f2c8f13fa73cc8690d63dae0cd993677f6aa47ae
Carlos Henrique <carlossoaressantana@hotmail.com>
2023-08-09 13:57:08 -0300

    refactor: remover arquivos não utilizados

--- report-service/src/main/java/br/com/henrique/repository/OpportunityRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.henrique.repository;
-
-import br.com.henrique.entity.OpportunityEntity;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class OpportunityRepository implements PanacheRepository<OpportunityEntity> {
-}
--- report-service/src/main/java/br/com/henrique/repository/QuotationRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.henrique.repository;
-
-import br.com.henrique.entity.QuotationEntity;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class QuotationRepository implements PanacheRepository<QuotationEntity> {
-}
